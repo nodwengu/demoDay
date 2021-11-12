55e1436d29f742ace121ab2f0965efe479b64ead
faizy.banda@gmail.com <faizy.banda@gmail.com>
2021-11-12 14:13:10 +0200

    with database

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
-const exphbs  = require('express-handlebars');
+const exphbs = require('express-handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 3018;
 app.use(express.static('public'));
+
+// import sqlite modules
+const sqlite3 = require('sqlite3');
+const { open } = require('sqlite');
 
 // enable the req.body object - to allow us to use HTML forms
 app.use(express.json());
@@ -12,11 +16,39 @@ app.use(express.urlencoded({ extended: false }));
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
+const users = [];
+
+open({
+	filename: './data.db',
+	driver: sqlite3.Database
+})
+
+//starting------
+
+app.get('/info', function (req, res) {
+    res.send(`please add, ${req.body.user1} ${req.body.user2}`);
+});
+//userName and details
+app.post('/info', function (req, res) {
+    console.log(req.body);
+    res.send(`Hello, ${req.body.FirstName} ${req.body.lastName}`);
+});
+//family members capturing
+app.post('/membz', function (req, res) {
+    console.log(req.body);
+    res.send(`Hello, ${req.body.FirstName} ${req.body.lastName}
+    ${req.body.age} ${req.body.race} ${req.body.main - address}
+    `);
+});
+
+//render query
+app.get('/users', function(req,res){
+res.render('users', users);
+});
 
 
 
-
-
+/////--------------------
 app.listen(PORT, function () {
     console.log(`App started on port ${PORT}`)
 });
---
+++ sqlite/sql.sql
@@ -0,0 +1,5 @@
+-- CREATE TABLE USERS( username TEXT NOT NULL PRIMARY KEY, 
+-- password NOT NULL, email NOT NULL, address NOT NULL);
+
+INSERT INTO users (username, password, email, address)
+VALUES ("mawande", "Mawande@2021", "mawande@gmail.com", "14, Alberton, 1400");
